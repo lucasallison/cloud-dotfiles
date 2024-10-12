fd04e20c5a645d07240d6bba6b8ff686efbc5c75
lucasallison <lucasgallison@gmail.com>
2024-10-12 19:54:13 +0200

    Added echos to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Install and source Nix
-curl -L https://nixos.org/nix/install | sh
+echo "Installing Nix..."
+if ! command -v nix &> /dev/null; then
+    curl -L https://nixos.org/nix/install | sh
+fi
 . ~/.nix-profile/etc/profile.d/nix.sh
 
+echo "Setting vim and zsh configurations..."
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 
+echo "Installing zsh syntax highlighting..."
 # Install zsh syntax highlighting
 if [ ! -d ~/.installs/zsh-syntax-highlighting ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
@@ -15,5 +19,5 @@ if [ ! -d ~/.installs/zsh-syntax-highlighting ]; then
 fi
 echo "source ~/.installs/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" > ~/.source_zsh_syntax_highlighting.sh
 
-echo "Start nix-shell:"
+echo "Everything should be configured! Start the nix-shell with:"
 echo "nix-shell $PWD/shell.nix"
