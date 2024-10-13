61b387acbc31ba36b702df69e41e718b70532d4b
lucasallison <lucasgallison@gmail.com>
2024-10-13 11:49:21 +0000

    Use variable for the configuration directory

--- install.sh
+++ install.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
+
 if ! command -v nix &> /dev/null; then
     echo "* Installing Nix..."
     curl -L https://nixos.org/nix/install | sh
     . ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
-if [ ! -d "~/.config/nix_shell" ]; then
-    mkdir -p ~/.config/nix_shell
+NIX_SHELL_CONFIG_DIR=~/.config/nix_shell
+
+if [ ! -d $NIX_SHELL_CONFIG_DIR ]; then
+    mkdir -p $NIX_SHELL_CONFIG_DIR
 fi
 
 echo "* Setting vim and zsh configurations..."
 cp default.vimrc ~/.vimrc
-cp default.zshrc ~/.config/nix_shell/.default.zshrc
+cp default.zshrc $NIX_SHELL_CONFIG_DIR/.default.zshrc
 
-if [ ! -f ~/.zshrc ] || ! grep -Fxq ". ~/.config/.default.zshrc" ~/.zshrc; then
-    echo ". ~/.config/.default.zshrc" >> ~/.zshrc
+if [ ! -f ~/.zshrc ] || ! grep -Fxq ". $NIX_SHELL_CONFIG_DIR/.default.zshrc" ~/.zshrc; then
+    echo ". $NIX_SHELL_CONFIG_DIR/.default.zshrc" >> ~/.zshrc
 fi
 
-echo "nix-shell $PWD/shell.nix" > ~/.config/nix_shell/.open_default_nix_shell.sh
+echo "nix-shell $PWD/shell.nix" > $NIX_SHELL_CONFIG_DIR/.open_default_nix_shell.sh
 
 # Install zsh syntax highlighting
 if [ ! -d ~/.installs/zsh-syntax-highlighting ]; then
     echo "* Installing zsh syntax highlighting..."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
-    mkdir -p ~/.installs
-    mv zsh-syntax-highlighting ~/.installs
+    mv zsh-syntax-highlighting $NIX_SHELL_CONFIG_DIR
 fi
 
-echo "source ~/.installs/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" > ~/.config/nix_shell/.source_zsh_syntax_highlighting.sh
+echo "source $NIX_SHELL_CONFIG_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" > $NIX_SHELL_CONFIG_DIR/.source_zsh_syntax_highlighting.sh
 
 echo "* Everything is configured!"
 nix-shell $PWD/shell.nix
